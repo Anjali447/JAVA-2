ec20d9b1ee618d14d9922bdcabbc8067b216ca10
Anjali Shrivastava <anjali.shrivastava.190@gmail.com>
2025-05-15 20:26:44 +0530

    Add files via upload

---
+++ New_Features1.java
@@ -0,0 +1,10 @@
+public class New_Features1 
+{
+    // no need to write strictfp on jdk17 onwards
+    strictfp public static void main(String[] args) {
+        float a = 5.2345f;
+        float b = 3.2949f;
+        float c = a / b;
+        System.out.println(c);
+    }
+}
